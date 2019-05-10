e3eabf7578416fdab465f7ada9f6f44d68436d0e
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2019-05-10 12:31:03 -0400

    Cree una variable a y la asigne

--- dot.py
+++ dot.py
@@ -8,6 +8,7 @@ print (env.observation_space, env.action_space)
 theta = np.zeros((env.action_space.shape[0], env.observation_space.shape[0]))
 deltas = [np.random.randn(*theta.shape) for k in range(num_deltas)]
 print ("Hola a todos")
+a = 56
 for i in range(8):
 	i += 2
 print(deltas)
