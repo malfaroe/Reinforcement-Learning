d5236b7de892ed26a1ab03e0c5740b93394a4cee
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2019-05-10 10:02:30 -0400

    Agregué un print de saludo en dot.py

--- dot.py
+++ dot.py
@@ -7,5 +7,5 @@ num_deltas = 3
 print (env.observation_space, env.action_space)
 theta = np.zeros((env.action_space.shape[0], env.observation_space.shape[0]))
 deltas = [np.random.randn(*theta.shape) for k in range(num_deltas)]
-
+print ("Hola a todos")
 print(deltas)
