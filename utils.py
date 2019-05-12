6eedfe29cea88f6a2fb5a26498843b2847b7e243
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2019-05-12 17:58:56 -0400

    Añado una ecuacion

--- utils.py
+++ utils.py
@@ -1,15 +1,4 @@
-from __future__ import print_function, division
-from builtins import range
-def print_values(V, g):
-  for i in range(g.width):
-    print("---------------------------")
-    for j in range(g.height):
-      v = V.get((i,j), 0)
-      if v >= 0:
-        print(" %.2f|" % v)
-      else:
-        print("%.2f|" % v) # -ve sign takes up an extra space
-    print("")
+
 
 def print_policy(P, g):
   for i in range(g.width):
@@ -33,3 +22,4 @@ def max_dict(d):
 print ("Saludos")
 a = 20
 print("este cambio l hice en la rama Test")
+a = a ** 2
